3620f6ef174e4facbe3dbdb859fa874e98a9c416
Md Mushfiqur Rahman <mushfiq.dustat@gmail.com>
2024-10-01 19:44:45 -0500

    Final version

--- Bike.py
+++ Bike.py
@@ -8,9 +8,12 @@ class Bike:
 # Number of gears (1-15) : set_number_of_gears(), get_number_of_gears()
 # Current gear (default value: 1): set_current_gear(), get_current_gear()
 # Number of wheels (1-4): set_number_of_wheels(), get_number_of_wheels()
+    __number_of_gear: int = 15
+    __number_of_wheels: int = 4
+    __current_gear: int = 1
+    __brake_type: str = "hand brake"
 
-
-    def __init__(self, number_of_gears=10, number_of_wheels=4, brake_type="hand brake"):
+    def __init__(self, number_of_gears=15, number_of_wheels=4, brake_type="hand brake", **temp):
 
     # validate number of gears
         if isinstance(number_of_gears, int) and 1 <= number_of_gears <= 15: # Ensures the value is an integer and value between 1 to 15
@@ -20,6 +23,9 @@ class Bike:
             self._number_of_gears = 10 # setting default value of the number of gears to 10
 
     # Current gear default to 1
+        if 'current_gear' in temp:
+            raise TypeError("'current_gear' is not an accepted keyword argument for Bike class."
+                            "\nThe default value is set to 1")
         self._current_gear = 1
 
     # Validate the number of wheels
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 
 from Bike import Bike
+honda = Bike()
 
-honda = Bike(number_of_gears=5, number_of_wheels=3, brake_type="hand brake")
-honda.increase_gear()
+print(honda)
+
+
 
 
 
